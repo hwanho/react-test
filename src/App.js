c358e1cf4b313dc1003edd58bd744ab051e18310
유환호 <yuhwanho@yuhwanhoui-MacBook-Pro.local>
2020-05-25 18:34:00 +0900

    컴포넌트 구조화

--- src/App.js
+++ src/App.js
@@ -2,21 +2,38 @@ import React from 'react';
 import './App.css';
 import Customer from './components/Customer';
 
-const customer = {
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
   'name': '테스트',
   'birthday': '910503',
   'gender': '남자',
   'job': '백수'
-} 
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '이순신',
+  'birthday': '920405',
+  'gender': '남자',
+  'job': '프로그래머'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '유관순',
+  'birthday': '950602',
+  'gender': '여자',
+  'job': '디자이너'
+}
+] 
 
 function App() {
   return (
-    <Customer 
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
+    <div> 
+      {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ); }) }
+    </div>
   );
 }
 
